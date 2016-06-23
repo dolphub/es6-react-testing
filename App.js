ccd09a2f814267c00b73633d4521e9e540c3da47
Randy Mikkelsaar <Randy Mikkelsaar>
2016-06-22 22:11:08 -0400

    lesson6 - widgets

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
 	constructor() {
 		super();
-		this.state = { txt: 'This is the state txt' };
+		this.state = { txt: '' };
+		this.update = this.update.bind(this);
 	}
 	update(e) {
 		this.setState({ txt: e.target.value });
@@ -11,12 +12,24 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-			<input type="text"
-				onChange={this.update.bind(this)} />
-			<h1>{this.state.txt}</h1>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		);
 	}
+}
+
+const Widget = (props) => {
+	return (
+		<div>
+		<input type="text"
+			onChange={props.update} />
+		<h1>{props.txt}</h1>
+		</div>
+	);
 }
 
 export default App
