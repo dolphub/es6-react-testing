49939c6949df711e0a88a014fa9eda10e0179ab9
Randy Mikkelsaar <Randy Mikkelsaar>
2016-06-22 22:48:08 -0400

    Component Lifecycle, exporting and showing mount cycle

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import App from './App';
+import Wrapper from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Wrapper />, document.getElementById('app'));
